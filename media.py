efee93addc317cd215148b7957b8ce297ae281df
haraldyy <69537970+haraldyy@users.noreply.github.com>
2020-09-09 13:41:34 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,12 @@
+def media():
+    media=0
+    for w in range(100):
+        c=int(input(''))
+        media+=c /100
+    print(f'{media:.2f}')
+
+def main():
+    media()
+if __name__=='__main__':
+    main()
+ 
